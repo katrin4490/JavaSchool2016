04b692b04e8561fa37b516c7b44921ca1f3b6a1b
admin <ekaterina_zhovnir@mail.ru>
2016-07-18 11:29:05 +0300

    1

--- src/ru/sbertech/homework/ReadString.java
+++ src/ru/sbertech/homework/ReadString.java
@@ -7,21 +7,20 @@ import java.util.*;
 public class ReadString {
     public static void main(String[] args) throws IOException {
         ArrayList<String> result = new ArrayList<String>();
-        File fd = new File("file.txt");
+        File fd = new File("txtfiles\\fileForReadString.txt");
         if (!fd.exists()) {
             System.out.print("File not exist");
             return;
         }
         Scanner fw = new Scanner(fd, "cp1251");
         while (fw.hasNextLine()) {
-            result.add(fw.nextLine());
+            result.add(fw.nextLine().replaceAll(" ",""));
         }
         fw.close();
         Collections.sort(result);
         System.out.println("After Sorting:");
-        for(String counter: result){
+        for (String counter : result) {
             System.out.println(counter);
         }
     }
 }
-
