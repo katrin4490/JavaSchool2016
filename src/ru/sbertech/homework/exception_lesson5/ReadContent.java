1d31b4621cd71c5117dd7bad9f7da8dd78b71a3f
admin <ekaterina_zhovnir@mail.ru>
2016-07-28 08:36:32 +0300

    28.07.2016

---
+++ src/ru/sbertech/homework/exception_lesson5/ReadContent.java
@@ -0,0 +1,47 @@
+package ru.sbertech.homework.exception_lesson5;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.net.HttpURLConnection;
+import java.util.*;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class ReadContent {
+    public static void main (String[] args)  {
+        System.out.println("Please enter URL");
+        Scanner sc = new Scanner(System.in);
+        if (!Read(new String(sc.nextLine()))){
+            System.out.println("Please enter an valid URL");
+            sc = new Scanner(System.in);
+            if (!Read(new String(sc.nextLine())))
+                System.out.println("Sorry, you have twice entered incorrect URL!");
+        }
+        sc.close();
+    }
+
+    public static boolean Read(String url2) {
+        URL url;
+        try {
+            url = new URL(url2);
+            HttpURLConnection connection = null;
+            if(!(url.openConnection() instanceof HttpURLConnection)) {
+                return false;
+            }
+            LineNumberReader reader = new LineNumberReader(new InputStreamReader(url.openStream()));
+            String current;
+            while((current = reader.readLine()) != null) {
+                System.out.println(current);
+            }
+            reader.close();
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
+
+
+
